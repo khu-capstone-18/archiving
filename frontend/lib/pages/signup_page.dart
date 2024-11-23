fa947ab022008a33746ad31823eb9f17a675d515
yhb457 <a01051603509@gmail.com>
2024-11-23 14:46:07 +0900

    signup_weight

--- frontend/lib/pages/signup_page.dart
+++ frontend/lib/pages/signup_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:frontend/services/api_service.dart';
 import 'package:frontend/pages/login_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class SignupPage extends StatefulWidget {
   @override
@@ -13,6 +14,7 @@ class _SignupPageState extends State<SignupPage> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
   String errorMessage = '';
 
   Future<void> signup() async {
@@ -21,16 +23,17 @@ class _SignupPageState extends State<SignupPage> {
         usernameController.text,
         passwordController.text,
         emailController.text,
+        weightController.text,
       );
 
       if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
         SharedPreferences prefs = await SharedPreferences.getInstance();
-        await prefs.setBool('first_login', true);
+        await prefs.setString('token', responseData['token']);
+        await prefs.setString('user_id', responseData['user_id']);
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(
-            builder: (context) => LoginPage(),
-          ),
+          MaterialPageRoute(builder: (context) => LoginPage()),
         );
       } else if (response.statusCode == 409) {
         setState(() {
@@ -71,6 +74,10 @@ class _SignupPageState extends State<SignupPage> {
             TextField(
               controller: emailController,
               decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: weightController,
+              decoration: InputDecoration(labelText: 'Weight'),
             ),
             SizedBox(height: 20),
             ElevatedButton(
